37ef0d89722da19eddb5fa106da9462961b1499e
Adam Jefferiss <adam.jefferiss@gmail.com>
2020-12-10 20:54:36 +0000

    Add comment

--- src/main/java/com/openmoments/scytale/encryption/CertificateFactory.java
+++ src/main/java/com/openmoments/scytale/encryption/CertificateFactory.java
@@ -1,7 +1,12 @@
 package com.openmoments.scytale.encryption;
 
-public class CertificateFactory {
+public final class CertificateFactory {
 
+    /***
+     * Generate a asymmetric certificate of type {@link com.openmoments.scytale.encryption.CertificateType CertificateType}
+     * @param certificateType Type of certificate to generate
+     * @return Instance of the concrete class to implement {@code certificateType}
+     */
     public AsymmetricCertificate get(CertificateType certificateType) {
         if (certificateType == null) {
             throw new IllegalArgumentException("Certificate type cannot be null");
