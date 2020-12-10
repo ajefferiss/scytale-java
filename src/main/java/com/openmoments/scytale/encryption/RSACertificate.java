745334f091b13967fc9530160fa1f8f93f75601d
Adam Jefferiss <adam.jefferiss@gmail.com>
2020-12-10 20:47:25 +0000

    Load / write to streams for RSA certificates

--- src/main/java/com/openmoments/scytale/encryption/RSACertificate.java
+++ src/main/java/com/openmoments/scytale/encryption/RSACertificate.java
@@ -1,21 +1,26 @@
 package com.openmoments.scytale.encryption;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.logging.Logger;
 
-/*
-https://www.novixys.com/blog/how-to-generate-rsa-keys-java/
- */
-
-public class RSACertificate implements AsymmetricCertificate {
+public final class RSACertificate implements AsymmetricCertificate {
 
     private static final Logger LOGGER = Logger.getLogger(RSACertificate.class.getName());
     private static final int DEFAULT_KEY_SIZE = 4096;
     private static final String ALGORITHM = "RSA";
     private int keySize = DEFAULT_KEY_SIZE;
 
+    /***
+     * Generate a RSA Keypair
+     * @return KeyPair
+     * @throws NoSuchAlgorithmException Indicates that a RSA certificate instance could not be created
+     */
     @Override
     public KeyPair generateKeyPair() throws NoSuchAlgorithmException {
         KeyPairGenerator kpg;
@@ -25,9 +30,52 @@ public class RSACertificate implements AsymmetricCertificate {
         return kpg.generateKeyPair();
     }
 
+    /***
+     * Specify the key length to use when creating certificates, defaults to 2048bit
+     * @param keyLength Key length value to use
+     * @return Instance of RSACertificate
+     */
     @Override
     public RSACertificate length(int keyLength) {
         this.keySize = keyLength;
         return this;
+    }
+
+    /***
+     * Generates a public and private key combination which are written to output streams
+     * @param privateKeyOutputStream Output stream for the private key
+     * @param publicKeyOutputStream Output stream for the public key
+     * @throws IOException Indicates an error writing to a output stream
+     * @throws NoSuchAlgorithmException Indicates that a RSA certificate instance could not be created
+     */
+    @Override
+    public void toStream(OutputStream privateKeyOutputStream, OutputStream publicKeyOutputStream) throws IOException, NoSuchAlgorithmException {
+        KeyPair keyPair = generateKeyPair();
+        privateKeyOutputStream.write(keyPair.getPrivate().getEncoded());
+        publicKeyOutputStream.write(keyPair.getPublic().getEncoded());
+    }
+
+    /***
+     * Create a KeyPair from input streams
+     * @param privateKeyInputStream Private key input stream
+     * @param publicKeyInputStream Public Key input stream
+     * @return KeyPair created
+     * @throws IOException Indicates error reading from the input stream
+     * @throws NoSuchAlgorithmException Indicates that a RSA certificate instance could not be created
+     * @throws InvalidKeySpecException Indicates that the RSA key was an invalid type
+     */
+    @Override
+    public KeyPair fromStream(InputStream privateKeyInputStream, InputStream publicKeyInputStream) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] privateKeyBytes = privateKeyInputStream.readAllBytes();
+        byte[] publicKeyBytes = publicKeyInputStream.readAllBytes();
+
+        KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+        PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+
+        X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+        PublicKey publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+
+        return new KeyPair(publicKey, privateKey);
     }
 }
--- src/test/java/com/openmoments/scytale/encryption/RSACertificateTest.java
+++ src/test/java/com/openmoments/scytale/encryption/RSACertificateTest.java
@@ -1,10 +1,17 @@
 package com.openmoments.scytale.encryption;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.InvalidKeySpecException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,5 +33,38 @@ class RSACertificateTest {
 
         assertEquals("RSA", keyPair.getPrivate().getAlgorithm());
         assertEquals(1024, privateKey.getModulus().bitLength());
+    }
+
+    @Test
+    void shouldWriteKeysToStreams(@TempDir Path tempDir) throws IOException, NoSuchAlgorithmException {
+        Path keyPath = tempDir.resolve("test.key");
+        Path pubPath = tempDir.resolve("test.pub");
+        OutputStream keyOutputStream = Files.newOutputStream(keyPath);
+        OutputStream pubOutputStream = Files.newOutputStream(pubPath);
+
+        new RSACertificate().toStream(keyOutputStream, pubOutputStream);
+
+        assertNotEquals(0, Files.size(keyPath));
+        assertNotEquals(0, Files.size(pubPath));
+    }
+
+    @Test
+    void shouldLoadKeysFromStreams(@TempDir Path tempDir) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        Path keyPath = tempDir.resolve("test.key");
+        Path pubPath = tempDir.resolve("test.pub");
+        OutputStream keyOutputStream = Files.newOutputStream(keyPath);
+        OutputStream pubOutputStream = Files.newOutputStream(pubPath);
+
+        new RSACertificate().toStream(keyOutputStream, pubOutputStream);
+
+        InputStream keyInputStream = Files.newInputStream(keyPath);
+        InputStream pubInputStream = Files.newInputStream(pubPath);
+
+        KeyPair loadedPair = new RSACertificate().fromStream(keyInputStream, pubInputStream);
+
+        assertNotNull(loadedPair);
+        assertNotEquals(0, loadedPair.getPrivate().getEncoded().length);
+        assertEquals("RSA", loadedPair.getPublic().getAlgorithm());
+        assertNotEquals(0, loadedPair.getPublic().getEncoded().length);
     }
 }
