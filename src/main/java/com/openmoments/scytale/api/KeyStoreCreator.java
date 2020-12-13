639041ae543be30b612027a209c91f753aeb4f67
Adam Jefferiss <adam.jefferiss@gmail.com>
2020-12-13 21:27:28 +0000

    Add implementation of byName to retrieve KeyStore

--- src/main/java/com/openmoments/scytale/api/KeyStoreCreator.java
+++ src/main/java/com/openmoments/scytale/api/KeyStoreCreator.java
@@ -1,17 +1,19 @@
 package com.openmoments.scytale.api;
 
+import com.openmoments.scytale.entities.KeyStore;
 import com.openmoments.scytale.exception.InvalidKeystoreException;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
 
 public class KeyStoreCreator {
 
     private Long id;
     private String name;
     private APIRequest apiRequest;
-
-    public KeyStoreCreator() {}
 
     /***
      * Sets the implementation of the {@link APIRequest APIRequest} interface
@@ -67,6 +69,29 @@ public class KeyStoreCreator {
     public KeyStore byId() throws IllegalArgumentException, InvalidKeystoreException, IOException, InterruptedException {
         KeyStoreRequest keyStoreRequest = new KeyStoreRequest(apiRequest);
         return fromJSON(new JSONObject(keyStoreRequest.getById(this.id)));
+    }
+
+    /***
+     * Create a keystore instance by search for an exact name match
+     * @return {@link KeyStore KeyStore}
+     * @throws InterruptedException - If the API operation is interrupted
+     * @throws InvalidKeystoreException - If the Keystore was not created correctly
+     * @throws IOException - If an I/O error occurs when sending or receiving API requests
+     */
+    public KeyStore byName() throws InterruptedException, InvalidKeystoreException, IOException {
+        KeyStoreRequest keyStoreRequest = new KeyStoreRequest(apiRequest);
+        JSONArray foundByName = new JSONArray(keyStoreRequest.searchByName(this.name));
+
+        Optional<JSONObject> jsonObject = StreamSupport.stream(foundByName.spliterator(), false)
+                                            .map(JSONObject.class::cast)
+                                            .filter(o -> o.get(KeyStoreRequest.KEYSTORE_NAME_ATTR).equals(this.name))
+                                            .findFirst();
+
+        if (jsonObject.isEmpty()) {
+            throw new InvalidKeystoreException("Keystore with name " + this.name + " does not exist");
+        }
+
+        return fromJSON(jsonObject.get());
     }
 
     /***
