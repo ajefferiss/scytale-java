a5f1164b7df0cbe7fee0ce1d0bff4e90646b2dab
Adam Jefferiss <adam.jefferiss@gmail.com>
2020-12-13 21:36:41 +0000

    Adding comments

--- src/main/java/com/openmoments/scytale/api/KeyStoreRequest.java
+++ src/main/java/com/openmoments/scytale/api/KeyStoreRequest.java
@@ -16,10 +16,22 @@ public class KeyStoreRequest {
 
     private APIRequest apiRequest;
 
+    /***
+     * Constructor requiring an APIRequest
+     * @param apiRequest - Implementation of APIRequest interface
+     */
     public KeyStoreRequest(APIRequest apiRequest) {
         this.apiRequest = apiRequest;
     }
 
+    /***
+     * Find a keystore based on the ID
+     * @param id - Id of the keystore to retrieve
+     * @return {@link String String} body of response from API
+     * @throws IOException - If an I/O error occurs when sending or receiving API requests
+     * @throws InterruptedException - If the API operation is interrupted
+     * @throws InvalidKeystoreException - If the API did not return a valid Keystore
+     */
     public String getById(Long id) throws InvalidKeystoreException, IOException, InterruptedException {
         validateID(id);
         validAPIImplementation();
@@ -34,7 +46,14 @@ public class KeyStoreRequest {
         return getResponse.body();
     }
 
-
+    /***
+     * Create KeyStore on the remote API
+     * @param name - Name of the keystore to create
+     * @return {@link String String} body of response from API
+     * @throws IOException - If an I/O error occurs when sending or receiving API requests
+     * @throws InterruptedException - If the API operation is interrupted
+     * @throws InvalidKeystoreException - If the API did not return a valid Keystore
+     */
     public String createKeyStore(String name) throws IOException, InterruptedException, InvalidKeystoreException {
         validateName(name);
         validAPIImplementation();
@@ -49,6 +68,14 @@ public class KeyStoreRequest {
         return createResponse.body();
     }
 
+    /***
+     * Updates a given keystore
+     * @param updated - New replacement keystore
+     * @return {@link String String} body of response from API
+     * @throws IOException - If an I/O error occurs when sending or receiving API requests
+     * @throws InterruptedException - If the API operation is interrupted
+     * @throws InvalidKeystoreException - If the API did not return a valid Keystore
+     */
     public String updateKeyStore(KeyStore updated) throws InvalidKeystoreException, IOException, InterruptedException {
         validateID(updated.getId());
         validateName(updated.getName());
@@ -68,6 +95,14 @@ public class KeyStoreRequest {
         return updateResponse.body();
     }
 
+    /***
+     * Retrieve a keystore based upon the name
+     * @param name - Name of keystore item
+     * @return {@link KeyStore KeyStore}
+     * @throws IOException - If an I/O error occurs when sending or receiving API requests
+     * @throws InterruptedException - If the API operation is interrupted
+     * @throws InvalidKeystoreException - If the API did not return a valid Keystore
+     */
     public String searchByName(String name) throws IOException, InterruptedException, InvalidKeystoreException {
         String searchURL = KEYSTORE_URI + "/search?name=" + name;
         HttpResponse<String> searchResponse = apiRequest.get(searchURL, null);
