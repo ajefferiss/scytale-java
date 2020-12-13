53e0c311994ca16a6a7f38d4a33275ef96e5008f
Adam Jefferiss <adam.jefferiss@gmail.com>
2020-12-13 13:08:42 +0000

    Gerneralise InvalidKeyStoreException text

--- src/main/java/com/openmoments/scytale/exception/InvalidKeystoreException.java
+++ src/main/java/com/openmoments/scytale/exception/InvalidKeystoreException.java
@@ -2,6 +2,6 @@ package com.openmoments.scytale.exception;
 
 public class InvalidKeystoreException extends Exception {
     public InvalidKeystoreException(String msg) {
-        super("Could not create a valid keystore " + msg);
+        super(msg);
     }
 }
