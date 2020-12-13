b2f529c7d33048db81c3d80395f7daf07f432d73
Adam Jefferiss <adam.jefferiss@gmail.com>
2020-12-13 21:27:47 +0000

    Implement UpdateKeystore

--- src/main/java/com/openmoments/scytale/api/KeyStoreRequest.java
+++ src/main/java/com/openmoments/scytale/api/KeyStoreRequest.java
@@ -1,5 +1,6 @@
 package com.openmoments.scytale.api;
 
+import com.openmoments.scytale.entities.KeyStore;
 import com.openmoments.scytale.exception.InvalidKeystoreException;
 import org.json.JSONObject;
 
@@ -20,12 +21,8 @@ public class KeyStoreRequest {
     }
 
     public String getById(Long id) throws InvalidKeystoreException, IOException, InterruptedException {
-        if (Optional.ofNullable(id).orElse(0L) == 0L) {
-            throw new IllegalArgumentException("Keystore ID cannot be empty");
-        }
-        if (apiRequest == null) {
-            throw  new IllegalArgumentException("API Request interface is required");
-        }
+        validateID(id);
+        validAPIImplementation();
 
         String getURL = KEYSTORE_URI + "/" + id;
 
@@ -37,13 +34,10 @@ public class KeyStoreRequest {
         return getResponse.body();
     }
 
+
     public String createKeyStore(String name) throws IOException, InterruptedException, InvalidKeystoreException {
-        if (name.isEmpty()) {
-            throw new IllegalArgumentException("Keystore name cannot be empty");
-        }
-        if (apiRequest == null) {
-            throw  new IllegalArgumentException("API Request interface is required");
-        }
+        validateName(name);
+        validAPIImplementation();
 
         JSONObject createJson = new JSONObject().put(KEYSTORE_NAME_ATTR, name);
         HttpResponse<String> createResponse = apiRequest.post(KEYSTORE_URI, createJson, null);
@@ -55,6 +49,25 @@ public class KeyStoreRequest {
         return createResponse.body();
     }
 
+    public String updateKeyStore(KeyStore updated) throws InvalidKeystoreException, IOException, InterruptedException {
+        validateID(updated.getId());
+        validateName(updated.getName());
+        validAPIImplementation();
+
+        JSONObject updateJson = new JSONObject()
+                .put(KEYSTORE_ID_ATTR, updated.getId())
+                .put(KEYSTORE_NAME_ATTR, updated.getName());
+
+        String updateURI = KEYSTORE_URI + "/" + updated.getId();
+        HttpResponse<String> updateResponse = apiRequest.put(updateURI, updateJson, null);
+
+        if (updateResponse.statusCode() != 200) {
+            throw new InvalidKeystoreException("API response failed with " + updateResponse.body());
+        }
+
+        return updateResponse.body();
+    }
+
     public String searchByName(String name) throws IOException, InterruptedException, InvalidKeystoreException {
         String searchURL = KEYSTORE_URI + "/search?name=" + name;
         HttpResponse<String> searchResponse = apiRequest.get(searchURL, null);
@@ -64,5 +77,23 @@ public class KeyStoreRequest {
         }
 
         return searchResponse.body();
+    }
+
+    private void validateID(Long id) {
+        if (Optional.ofNullable(id).orElse(0L) == 0L) {
+            throw new IllegalArgumentException("Keystore ID cannot be empty");
+        }
+    }
+
+    private void validateName(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Keystore name cannot be empty");
+        }
+    }
+
+    private void validAPIImplementation() {
+        if (apiRequest == null) {
+            throw  new IllegalArgumentException("API Request interface is required");
+        }
     }
 }
