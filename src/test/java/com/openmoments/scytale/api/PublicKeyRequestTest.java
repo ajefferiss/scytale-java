0b51ad38a67e901bf53020d0984be670f2bfd952
Adam Jefferiss <adam.jefferiss@gmail.com>
2020-12-24 12:32:34 +0000

    Update tests to provide valid public key

--- src/test/java/com/openmoments/scytale/api/PublicKeyRequestTest.java
+++ src/test/java/com/openmoments/scytale/api/PublicKeyRequestTest.java
@@ -4,14 +4,21 @@ import com.openmoments.scytale.TestUtils;
 import com.openmoments.scytale.entities.KeyStore;
 import com.openmoments.scytale.entities.ScytalePublicKey;
 import com.openmoments.scytale.exception.ScytaleException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.cert.CertificateException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -44,12 +51,15 @@ class PublicKeyRequestTest {
         @Test
         @DisplayName("Should return PublicKey list")
         void shouldReturnKeysAsList() throws IOException, InterruptedException, ScytaleException, CertificateException {
-            when(apiRequest.get(eq("keystores/1/keys")))
-                .thenReturn(TestUtils.setupHTTPResponse(200, "[{\"id\": 1, \"publicKey\": \"Test Updated\"}]"));
+            String testPublicKey = readTestFile("test_rsa_public.pem");
+            JSONObject resultObject = new JSONObject().put("id", 1).put("publicKey", testPublicKey);
+            JSONArray resultArray = new JSONArray().put(resultObject);
+
+            when(apiRequest.get(eq("keystores/1/keys"))).thenReturn(TestUtils.setupHTTPResponse(200, resultArray.toString()));
 
             List<ScytalePublicKey> getResponse = new PublicKeyRequest(apiRequest).getAll(keyStore);
             List<ScytalePublicKey> expectedList = new ArrayList<>();
-            expectedList.add(new ScytalePublicKey(1L, "Test Updated"));
+            expectedList.add(new ScytalePublicKey(1L, testPublicKey));
 
             assertEquals(expectedList, getResponse);
         }
@@ -85,11 +95,13 @@ class PublicKeyRequestTest {
         @Test
         @DisplayName("Should return PublicKey added")
         void shouldReturnPublicKeyAdded() throws IOException, InterruptedException, ScytaleException, CertificateException {
-            when(apiRequest.post(eq("keystores/1/keys"), any()))
-                .thenReturn(TestUtils.setupHTTPResponse(200, "{\"id\": 1, \"publicKey\": \"Test Public Key\"}"));
+            String testPublicKey = readTestFile("test_rsa_public.pem");
+            JSONObject resultObject = new JSONObject().put("id", 1).put("publicKey", testPublicKey);
 
-            ScytalePublicKey scytalePublicKeyAdded = new PublicKeyRequest(apiRequest).add("Test Public Key", keyStore);
-            ScytalePublicKey added = new ScytalePublicKey(1L, "Test Public Key");
+            when(apiRequest.post(eq("keystores/1/keys"), any())).thenReturn(TestUtils.setupHTTPResponse(200, resultObject.toString()));
+
+            ScytalePublicKey scytalePublicKeyAdded = new PublicKeyRequest(apiRequest).add(testPublicKey, keyStore);
+            ScytalePublicKey added = new ScytalePublicKey(1L, testPublicKey);
 
             assertEquals(added, scytalePublicKeyAdded);
         }
@@ -101,7 +113,8 @@ class PublicKeyRequestTest {
         @Test
         @DisplayName("Should throw Scytale Exception on error")
         void shouldThrowOnError() throws IOException, InterruptedException, CertificateException {
-            ScytalePublicKey newScytalePublicKey = new ScytalePublicKey(1L, "Test");
+            ScytalePublicKey newScytalePublicKey = new ScytalePublicKey(1L, readTestFile("test_rsa_public.pem"));
+
             when(apiRequest.put(eq("keystores/1/keys/1"), any()))
                     .thenReturn(TestUtils.setupHTTPResponse(200, "Not JSON"));
 
@@ -114,13 +127,25 @@ class PublicKeyRequestTest {
         @Test
         @DisplayName("Should return PublicKey added")
         void shouldReturnPublicKeyUpdated() throws IOException, InterruptedException, ScytaleException, CertificateException {
-            ScytalePublicKey updatedScytalePublicKey = new ScytalePublicKey(1L, "Updated Test Public Key");
-            when(apiRequest.put(eq("keystores/1/keys/1"), any()))
-                    .thenReturn(TestUtils.setupHTTPResponse(200, "{\"id\": 1, \"publicKey\": \"Updated Test Public Key\"}"));
+            String testPublicKey = readTestFile("test_rsa_public.pem");
+            JSONObject resultObject = new JSONObject().put("id", 1).put("publicKey", testPublicKey);
+            ScytalePublicKey updatedScytalePublicKey = new ScytalePublicKey(1L, testPublicKey);
+
+            when(apiRequest.put(eq("keystores/1/keys/1"), any())).thenReturn(TestUtils.setupHTTPResponse(200, resultObject.toString()));
 
             ScytalePublicKey scytalePublicKeyAdded = new PublicKeyRequest(apiRequest).update(updatedScytalePublicKey, keyStore);
 
             assertEquals(updatedScytalePublicKey, scytalePublicKeyAdded);
         }
+    }
+
+    String readTestFile(String resourcePath) {
+        try {
+            Path path = Paths.get(getClass().getClassLoader().getResource(resourcePath).toURI());
+            return Files.lines(path).collect(Collectors.joining("\n"));
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
